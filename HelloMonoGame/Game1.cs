fb311b5d25616e31c9a6270918c6e7c26a69615d
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2019-11-24 12:04:25 +0200

    Test moving ball

--- HelloMonoGame/Game1.cs
+++ HelloMonoGame/Game1.cs
@@ -10,6 +10,7 @@ namespace HelloMonoGame
         private SpriteBatch _spriteBatch;
         private SpriteFont _spriteFont;
 
+        private SpriteBall _spriteBall;
         public Game1()
         {
             _graphicsDeviceManager = new GraphicsDeviceManager(this);
@@ -20,7 +21,12 @@ namespace HelloMonoGame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _spriteFont = Content.Load<SpriteFont>("Arial");
+            //_spriteFont = Content.Load<SpriteFont>("Arial");
+
+            var texture = Content.Load<Texture2D>("CircleBlue");
+
+            _spriteBall = new SpriteBall(texture);
+            _spriteBall.position = new Vector2(100, 100);
         }
 
         protected override void UnloadContent()
@@ -29,12 +35,15 @@ namespace HelloMonoGame
 
         protected override void Update(GameTime gameTime)
         {
-            var keyboardState = Keyboard.GetState();
+            //var keyboardState = Keyboard.GetState();
 
-            if (keyboardState.IsKeyDown(Keys.Escape))
-                Exit();
+            //if (keyboardState.IsKeyDown(Keys.Escape))
+            //    Exit();
+
+            _spriteBall.Update();
 
             base.Update(gameTime);
+
         }
 
         protected override void Draw(GameTime gameTime)
@@ -42,7 +51,10 @@ namespace HelloMonoGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
-            _spriteBatch.DrawString(_spriteFont, "Hello MonoGame!", new Vector2(100, 100), Color.White);
+
+            _spriteBall.Draw(_spriteBatch);
+
+           // _spriteBatch.DrawString(_spriteFont, "Hello MonoGame!", new Vector2(100, 100), Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ HelloMonoGame/SpriteBall.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMonoGame
+{
+    public class SpriteBall
+    {
+        private Texture2D texture2D;
+        public Vector2 position;
+        public float speed = 2f;
+
+        public SpriteBall(Texture2D texture2D)
+        {
+            this.texture2D = texture2D;
+        }
+
+        public void Update()
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                position.Y -= speed;
+            }
+            else if(Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                position.Y += speed;
+            }
+            else if(Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                position.X += speed;
+            }
+            else if(Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                position.X -= speed;
+            }
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture2D,position,Color.Black);
+        }
+    }
+}
